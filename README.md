6d5d3a1ac803213746d0a5ed387bd733149dd223
Daniil <dsbl@lodossteam.com>
2015-12-01 14:55:51 +0300

    optimist help and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Client
+
+### Required
+
+Check that you have installed:
+
++   nodejs
+
+### Run
+
+For start client:
+
+1. 	go to project directory
+1.  run `$ npm install`
+1.  run `$ node index -h` to get help
+1.  run `$ node index` to start client
+
+###
+
+This app was tested under Ubuntu 14.04.3 LTS (trusty), Mac OS X 10.10.5 (Yosemite)
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var fs        = require('fs');
 var net       = require('net');
 var path      = require('path');
-var argv      = require('optimist').argv;
+var optimist  = require('optimist');
 var node_uuid = require('node-uuid');
 
-var config = {
+optimist.usage('Usage: $0 [-v] [--rh] [--rp] [--lh] [--lp] [--ps] [--uuid]');
+optimist.alias('h', 'help');
+optimist.alias('v', 'verbose');
+optimist.describe('h', 'show this help');
+optimist.describe('lh', 'local server address (default localhost)');
+optimist.describe('lp', 'local server port (default 3001)');
+optimist.describe('ps', 'socket pool size (default 10)');
+optimist.describe('rh', 'remote server address (default localhost)');
+optimist.describe('rp', 'remote server port (default 5000)');
+optimist.describe('uuid', 'path to uuid file (default ./uuid)');
+optimist.describe('v', 'enable verbose mode');
+
+var uuid;
+var argv    = optimist.argv;
+var config  = {
   local_server: {
     host: argv.lh || 'localhost',
     port: argv.lp || 3001
@@ -18,8 +32,10 @@ var config = {
   debug: argv.v || argv.verbose || false
 };
 
-var uuid;
-
+if (argv.h || argv.help) {
+  optimist.showHelp();
+  process.exit(0);
+}
 if (fs.existsSync(config.uuid_file)) {
   uuid = fs.readFileSync(config.uuid_file).toString();
 } else {
@@ -27,6 +43,8 @@ if (fs.existsSync(config.uuid_file)) {
   fs.writeFileSync(config.uuid_file, uuid);
 }
 console.debug = config.debug ? console.log.bind(console) : function () {};
+
+console.log('Your id is ' + uuid);
 
 function createPoolConnection () {
   var remote_socket = net.connect(config.remote_server.port, config.remote_server.host, function() {
@@ -53,6 +71,10 @@ var remote_client = net.connect(config.remote_server.port, config.remote_server.
   for (var i = 0; i < 10; i++) {
     createPoolConnection();
   }
+});
+
+remote_client.on('error', function() {
+  console.error('Could not connect to remote server.');
 });
 
 remote_client.on('end', function() {
